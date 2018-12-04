336c977cab395f78687b3425ab8e2f27a3f7f2be
mariusAlc <mannye.skps@gmail.com>
2018-12-04 15:18:08 +0200

    SearchBar

--- src/ui/js/components/shop/Shop.vue
+++ src/ui/js/components/shop/Shop.vue
@@ -26,15 +26,15 @@
 		</li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="searchToken" >
+      <button class="btn btn-outline-success my-2 my-sm-0" type="button">Search</button>
     </form>
   </div>
 </nav>
 <div class="row">
-<div class="col-md-3" v-for="(item,index) in forSale" :key=index>
+<div class="col-md-3" v-for="(item,index) in filteredItems" :key="index">
   <div class="card">
-    <div :id="'car'+item.invId" class="carousel slide" data-ride="carousel">
+    <div :id="'car'+item.id" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
 			<div class="carousel-item active">
 			<img class="d-block w-100" src="../../../img/generic-blue-ridge-mountains-2528x1422.jpg" alt="First slide">
@@ -46,11 +46,11 @@
 			<img class="d-block w-100" src="../../../img/Monasterio_Khor_Virap,_Armenia,_2016-10-01,_DD_25.jpg" alt="Third slide">
 			</div>
 		</div>
-		<a class="carousel-control-prev" :href="'#car'+item.invId"  role="button" data-slide="prev">
+		<a class="carousel-control-prev" :href="'#car'+item.id"  role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="carousel-control-next" :href="'#car'+item.invId"  role="button" data-slide="next">
+		<a class="carousel-control-next" :href="'#car'+item.id"  role="button" data-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
@@ -71,7 +71,14 @@
 <script>
 
 var Loading = require('../Loading.vue');
-
+class asset {
+	constructor (id, name, image, price){
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.price = price;
+	}
+}
 module.exports = {
 	name: 'Shop',
 
@@ -81,18 +88,23 @@ module.exports = {
 	
 	data() {
 		return {
-			forSale: [
-				{ invId: 1, name: 'Lorem ipsum', image: '//placehold.it/200', price: 999 },
-				{ invId: 2, name: 'Lorem ipsum', image: '//placehold.it/200', price: 1499 },
-				{ invId: 3, name: 'Lorem ipsum', image: '//placehold.it/200', price: 499 },
-				{ invId: 4, name: 'Lorem ipsum', image: '//placehold.it/200', price: 299 },
-				{ invId: 5, name: 'Lorem ipsum', image: '//placehold.it/200', price: 699 },
+			searchToken:'',
+			forSale : [
+				new asset('1', 'Loreeeem ipsum', '//placehold.it/200',  '999'),
+				new asset('2', 'Loremmmm', '//placehold.it/200',  '1499'),
+				new asset('3', 'Ipsum', '//placehold.it/200',  '499'),
+				new asset('4', 'Loremipsum', '//placehold.it/200',  '299'),
+				new asset('5', 'LoremIpsum', '//placehold.it/200',  '699')
 			],
+			noSearch:true,
 		};
 	},
-
-	methods: {
-		
+	computed: {
+		filteredItems() {
+			return this.forSale.filter( item => {
+				return item.name.toLowerCase().includes(this.searchToken.toLowerCase());
+			});
+		},
 	}
 };
 
