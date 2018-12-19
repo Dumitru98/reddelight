f55a594365270c9ad4bee5d47613b50fdcfa2fd4
Dumitru98 <dumitru.alexandru.1998@gmail.com>
2018-12-19 19:02:17 +0200

    codestyle

--- src/ui/js/components/controlPanel/ControlPanel.vue
+++ src/ui/js/components/controlPanel/ControlPanel.vue
@@ -3,9 +3,9 @@
 	<div class="container">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<a class="navbar-brand" href="dashboard.html">Red Delight</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
@@ -39,15 +39,18 @@
 				</ul>
 			</div>
 		</nav>
+
 		<form class="form-group">
 			<h2>Add Product</h2>
 			<input class="form-control" v-model="testName" placeholder="Name">
 			<input class="form-control" v-model="testPrice" placeholder="Price">
 			<input class="form-control" v-model="testStock" placeholder="Stock">
+
 			<label for="categoryInput">Category</label>
 			<select v-model="testCategory" id="categoryInput">
 				<option v-for="(item,index) in Categories" :key=index :value="item">{{item}}</option>
 			</select>
+
 			<ul>
 				<li v-for="(category, index) in CategoriesToShow" :key="index">
 					<p>{{category}}<button type="button" @click="outWithCategory(category)">x</button></p>
@@ -55,6 +58,7 @@
 			</ul>
 			<button @click="addCategory" class="btn btn-inline" type="button">Add Category</button>
 		</form>
+
 		<button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="createProduct()">Add Product</button>
 
 		<form>
@@ -67,22 +71,27 @@
 								<div class="carousel-item active">
 									<img class="d-block w-100" src="../../../img/generic-blue-ridge-mountains-2528x1422.jpg" alt="First slide">
 								</div>
+
 								<div class="carousel-item">
 									<img class="d-block w-100" src="../../../img/josh-kao-genericmountains.jpg" alt="Second slide">
 								</div>
+
 								<div class="carousel-item">
 									<img class="d-block w-100" src="../../../img/Monasterio_Khor_Virap,_Armenia,_2016-10-01,_DD_25.jpg" alt="Third slide">
 								</div>
 							</div>
+
 							<a class="carousel-control-prev" :href="'#car'+item.id"  role="button" data-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 								<span class="sr-only">Previous</span>
 							</a>
+
 							<a class="carousel-control-next" :href="'#car'+item.id"  role="button" data-slide="next">
 								<span class="carousel-control-next-icon" aria-hidden="true"></span>
 								<span class="sr-only">Next</span>
 							</a>
 						</div>
+
 						<div class="card-body">
 							<h4 class="card-title" @click="productPage(item.id)">{{ item.name }}</h4>
 							<div class="card-text" @click="getProducts()">{{ item.price / 100 }} Lei</div>
@@ -152,30 +161,37 @@ module.exports = {
 			productToChange:'',
 		};
 	},
-	async created(){
+
+	async created() {
 		let state = await this.$store.dispatch('category/all');
 		console.log('NOTOKAY');
+
 		if(state){
 			console.log(state);
+
 			for(let category of state) {
 				this.Categories.push(category.name);
 			}
+
 			this.Categories.sort();
 		}
-		let products = await this.$store.dispatch('product/get30',1);
+
+		let products = await this.$store.dispatch('product/get30', 1);
+
 		for(let product of products) {
-			this.products.push(new asset(product.id,product.name,'//placehold.id/200',product.price));
+			this.products.push(new asset(product.id, product.name, '//placehold.id/200', product.price));
 		}
 	},
 
 	methods: {
-		addCategory(){
+		addCategory() {
 			this.CategoriesToSend.push(this.testCategory);
 			this.CategoriesToShow.push(this.testCategory);
 		},
 
 		outWithCategory(category) {
 			var index = this.CategoriesToShow.indexOf(category);
+
 			if(index > -1) {
 				this.CategoriesToShow.splice(index, 1);
 				this.CategoriesToSend.splice(index, 1);
@@ -186,7 +202,7 @@ module.exports = {
 			await this.$store.dispatch('category/create', this.testCategory);
 		},
 
-		async createProduct(){
+		async createProduct() {
 			await this.$store.dispatch('product/create',{
 				name:this.testName, 
 				price:this.testPrice, 
@@ -195,7 +211,7 @@ module.exports = {
 			});
 		},
 		
-		async removeProduct(idToDelete){
+		async removeProduct(idToDelete) {
 			await this.$store.dispatch('product/delete', idToDelete);
 		}
 	}
